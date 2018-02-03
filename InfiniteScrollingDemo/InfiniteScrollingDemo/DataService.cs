6904010db82525bf73757bdd70aef459e46ee0dd
webmasterdevlin <webmasterdevlin@gmail.com>
2018-02-03 13:38:22 +0800

    Add project files.

---
+++ InfiniteScrollingDemo/InfiniteScrollingDemo/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteScrollingDemo
+{
+    public class DataService
+    {
+        private readonly List<string> _data = new List<string>
+        {
+            "Sasuke", "Kakashi", "Naruto", "Madara", "Sakura", "Itachi", "Orochimaru", "Minato", "Obito",
+            "Kiba", "Tsunade", "Hashirama", "Pain", "Jiraiya", "Rock Lee", "Shiakamaru", "Hinata", "Gaara",
+            "Neji", "Kabuto", "Sarotobi", "Shino", "Guy", "Choji", "Ino", "Kurama",
+            "Temari", "Killer Bee", "Sasori", "Kisame", "Tobirama", "Danzo", "Tenten", "Kaguya", "Konohamaru",
+            "Zetsu", "Deidara", "Asuma", "Zabuza", "Sarada", "Kushina", "Karin", "Iruka", "Sai", "Konan",
+            "Kankuro", "Yamato", "Haku", "Hidan", "Kurenai", "Kakazu",
+        };
+
+        public async Task<List<string>> GetItemsAsync(int pageIndex, int pageSize)
+        {
+            await Task.Delay(2000);
+
+            return _data.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        }
+    }
+}
---
+++ InfiniteScrollingDemo/InfiniteScrollingDemo/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Extended;
+
+namespace InfiniteScrollingDemo
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private const int PageSize = 10;
+        private readonly DataService _dataService = new DataService();
+        public InfiniteScrollCollection<string> Items { get; }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Items = new InfiniteScrollCollection<string>
+            {
+                OnLoadMore = async () =>
+                {
+                    IsBusy = true;
+                    int page = Items.Count / PageSize;
+                    List<string> items = await _dataService.GetItemsAsync(page, PageSize);
+                    IsBusy = false;
+                    return items;
+                },
+                OnCanLoadMore = () => Items.Count < 44
+            };
+            DownloadDataAsync();
+        }
+
+        private async Task DownloadDataAsync()
+        {
+            List<string> items = await _dataService.GetItemsAsync(0, PageSize);
+            Items.AddRange(items);
+        }
+    }
+}
